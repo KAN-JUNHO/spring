2c871e263abde61bec39a77a50b43d8855d8aed7
java <https://github.com/KAN-JUNHO/spring.git>
2020-01-07 17:57:33 +0900

    ...

---
+++ ch02/src/main/java/kr/co/ch02/sub3/DiTest3.java
@@ -0,0 +1,32 @@
+package kr.co.ch02.sub3;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import kr.co.ch02.sub3.LgTV;
+import kr.co.ch02.sub3.SamsungTV;
+import kr.co.ch02.sub3.TV;
+
+/*
+ * 날짜 : 2020/01/07
+ * 이름 : 김철학
+ * 내용 : 어노테이션 DI 실습하기
+ */
+public class DiTest3 {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext ctx = new GenericXmlApplicationContext("spring-context.xml");
+		
+		TV tv5 = (LgTV) ctx.getBean("tv5");
+		tv5.power();
+		tv5.chUp();
+		tv5.soundUp();
+		
+		TV tv6 = (SamsungTV) ctx.getBean("tv6");
+		tv6.power();
+		tv6.chUp();
+		tv6.soundUp();
+		
+	}
+}
