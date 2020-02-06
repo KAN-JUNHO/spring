5bd75a8dc5d345802ab7039105ed98230742dd71
java <https://github.com/KAN-JUNHO/android.git>
2020-02-06 17:48:10 +0900

    2020/02/06

--- jcinema/src/main/java/kr/co/jcinema/service/TicketingService.java
+++ jcinema/src/main/java/kr/co/jcinema/service/TicketingService.java
@@ -1,8 +1,26 @@
 package kr.co.jcinema.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import kr.co.jcinema.admin.vo.AdminTicketVo;
+import kr.co.jcinema.dao.TicketingDao;
+import kr.co.jcinema.vo.SeatVo;
 
 @Service
 public class TicketingService {
 
+	private TicketingDao dao;
+	
+	public List<SeatVo> selecltSeat(AdminTicketVo vo){
+		
+		List<SeatVo> seatList = dao.selectSeat(vo);
+		
+		seatList.subList(fromIndex, toIndex)
+		
+		return seatList;
+	}
 }
+	
+	
