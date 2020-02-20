ef554c8279cf85a3a683f4b0e0090df7eb7cfee9
java <https://github.com/KAN-JUNHO/android.git>
2020-02-20 17:49:35 +0900

    2020/02/20

---
+++ kan/src/main/java/kr/co/kan/controller/TicketController.java
@@ -0,0 +1,16 @@
+package kr.co.kan.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TicketController {
+	
+	@GetMapping(value={"/ticket"})
+	public String choiceMovie() {
+		return "ticket";
+	}
+	
+
+	
+}
