69e474516397a479f0c77143be54e6b738d12f6e
java <https://github.com/KAN-JUNHO/android.git>
2020-03-09 10:14:09 +0900

    2020/03/09

---
+++ kan/src/main/java/kr/co/kan/admin/service/AdminMemberService.java
@@ -0,0 +1,8 @@
+package kr.co.kan.admin.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminMemberService {
+
+}
---
+++ kan/src/main/java/kr/co/kan/service/MemberService.java
@@ -0,0 +1,33 @@
+package kr.co.kan.service;
+
+import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import kr.co.kan.dao.MemberDao;
+import kr.co.kan.vo.MemberVo;
+
+@Service
+public class MemberService {
+	
+	@Autowired
+	private MemberDao dao;
+	
+	public void insertMember(MemberVo vo) {
+		dao.insertuser(vo);
+	}
+	
+	//로그인 창
+	public MemberVo login(MemberVo vo) throws Exception {
+		return dao.login(vo);
+	}	
+	public List<MemberVo> selectMembers(){
+		return dao.selectMembers();
+	}
+}
